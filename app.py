f22b3d384d5abc9d4dca67a1108051f5a30900ef
erayozer17 <eray.ozer.91@gmail.com>
2020-03-02 23:36:40 +0100

    deploy test fix

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World2!'
 
+@app.route('/sanity')
+def hello_world():
+    return 'deploy done!!!'
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
